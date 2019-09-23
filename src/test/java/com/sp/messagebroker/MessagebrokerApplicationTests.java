fd77869e23f16d175542ae87aa2975292e71c97d
seanjay10k <31979155+seanjay10k@users.noreply.github.com>
2019-09-23 07:32:18 -0500

    Update MessagebrokerApplicationTests.java

--- src/test/java/com/sp/messagebroker/MessagebrokerApplicationTests.java
+++ src/test/java/com/sp/messagebroker/MessagebrokerApplicationTests.java
@@ -16,17 +16,8 @@ public class MessagebrokerApplicationTests {
 	@Test
 	public void timestampsParseCorrectly() 
 	{
-		// Format: EEE MMM dd HH:mm:ss zzz yyyy	
-		Position position = new Position("23", "21", "Monday January 09 16:15:00 +0000 2017");
+		
+	}
+
 	
-		// would throw exception if invalid
-	}
-
-	@Test(expected=java.lang.RuntimeException.class)
-	public void invalidTimestampsFailCorrectly() 
-	{
-		// Format: EEE MMM dd HH:mm:ss zzz yyyy		
-		Position position = new Position("23", "21", "Monday January 75 aa:15:00 +0000 2017");
-	}
-
 }
