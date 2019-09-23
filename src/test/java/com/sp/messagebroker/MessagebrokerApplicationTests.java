ef1f5eb38b3c7a7f830ba4871ef07ddd3e56ef30
seanjay10k <31979155+seanjay10k@users.noreply.github.com>
2019-09-23 07:29:32 -0500

    Update MessagebrokerApplicationTests.java

--- src/test/java/com/sp/messagebroker/MessagebrokerApplicationTests.java
+++ src/test/java/com/sp/messagebroker/MessagebrokerApplicationTests.java
@@ -5,12 +5,28 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class MessagebrokerApplicationTests {
 
+	//@Test
+	//public void contextLoads() {
+	//}
+	
 	@Test
-	public void contextLoads() {
+	public void timestampsParseCorrectly() 
+	{
+		// Format: EEE MMM dd HH:mm:ss zzz yyyy	
+		Position position = new Position("23", "21", "Monday January 09 16:15:00 +0000 2017");
+	
+		// would throw exception if invalid
+	}
+
+	@Test(expected=java.lang.RuntimeException.class)
+	public void invalidTimestampsFailCorrectly() 
+	{
+		// Format: EEE MMM dd HH:mm:ss zzz yyyy		
+		Position position = new Position("23", "21", "Monday January 75 aa:15:00 +0000 2017");
 	}
 
 }
